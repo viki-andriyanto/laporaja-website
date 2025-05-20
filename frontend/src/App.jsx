95a8782eafd73aff6e6e714b38a1777b3855f846
Ardikaoktavian <ardikaardika17436@gmail.com>
2025-05-20 16:12:36 +0700

    Pembuatan struktur file (sementara)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,29 +7,26 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* {NAVBAR} BERISI KODE UNTUK NAVBAR */}
+
+        {/* {ROUTING} BERISI KODE UNTUK ROUTE*/}
+
+
+        {/* {FOOTER} BERISI KODE MEMANGGIL ROUTE*/}
+        {/* CONTOH 
+          <footer />*/}
+
+        {/* {MODAL LOGIN} BERISI KODE UNTUK LOGIN*/}
+
+
+        {/* {MODAL REGISTER} BERISI KODE UNTUK REGISTER*/}
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </Router>  
+  );
+};
 
 export default App
